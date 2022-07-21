eaa86795fa132333bc1fb9ed34b0fa704b48f80d
Brice Grenard <brice.grenard@gmail.com>
2022-07-21 09:27:46 +0800

    Some refactoring in LotteryParticipation component

--- components/LotteryParticipation.js
+++ components/LotteryParticipation.js
@@ -2,84 +2,142 @@ import { useEffect, useState } from "react";
 import { useWeb3Contract, useMoralis } from "react-moralis";
 import { contractAddresses, contractAbi } from "../contracts_constants/index";
 import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
+
+var deployedAddress;
 
 export default function Participate() {
     const { Moralis, chainId, isWeb3Enabled } = useMoralis();
     const { runContractFunction } = useWeb3Contract();
     const [participationFee, setParticipationFee] = useState("0");
     const [chainIdStr, setChainId] = useState("1");
-    const [contractAddress, setcontractAddress] = useState(null);
+    const [numberOfPlayers, setNumberOfPlayers] = useState("0");
 
-    var lotteryContractAddress = null;
+    const dispatch = useNotification();
+
+    function isChainIdSupported(chainIdParam) {
+        return chainIdParam in contractAddresses;
+    }
+
+    function getLotteryDeployedAddress(chainIdParam) {
+        var chainIdStr = parseInt(chainIdParam).toString();
+        if (isChainIdSupported(chainIdStr)) {
+            return contractAddresses[chainIdStr][0];
+        }
+        return null;
+    }
 
     const participateToLottery = async () => {
         const optionsParticipate = {
             abi: contractAbi,
-            contractAddress: contractAddress,
+            contractAddress: deployedAddress,
             functionName: "participate",
             params: {},
             msgValue: participationFee,
         };
-        console.log(optionsParticipate);
+
         await runContractFunction({
             params: optionsParticipate,
+            onSuccess: (tx) => handleSuccessTxParticipate(tx),
+            onError: (error) => console.log(error),
         });
     };
 
-    const fetchParticipationFee = async (fromChain) => {
-        const newChainIdStr = parseInt(fromChain).toString();
+    const handleSuccessTxParticipate = async (tx) => {
+        console.log(tx);
+        await tx.wait(1);
+        handleNotification(tx);
+        await fetchNumberOfParticipants();
+    };
 
-        if (newChainIdStr in contractAddresses)
-            lotteryContractAddress = contractAddresses[newChainIdStr][0];
-        else lotteryContractAddress = null;
+    const handleNotification = (tx) => {
+        dispatch({
+            type: "info",
+            message: "Transaction completed!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        });
+    };
 
-        setChainId(newChainIdStr);
-        setcontractAddress(lotteryContractAddress);
+    const fetchParticipationFee = async () => {
+        const optionsGetParticipationFee = {
+            abi: contractAbi,
+            contractAddress: deployedAddress,
+            functionName: "getParticipationFee",
+            params: {},
+        };
 
-        if (lotteryContractAddress != null) {
-            const optionsGetParticipationFee = {
-                abi: contractAbi,
-                contractAddress: lotteryContractAddress,
-                functionName: "getParticipationFee",
-                params: {},
-            };
-            const lotteryParticipationFee = await runContractFunction({
-                params: optionsGetParticipationFee,
-            });
+        const lotteryParticipationFee = await runContractFunction({
+            params: optionsGetParticipationFee,
+        });
 
-            setParticipationFee(lotteryParticipationFee);
-        }
+        setParticipationFee(lotteryParticipationFee);
+    };
+
+    const fetchNumberOfParticipants = async () => {
+        const optionsGetNumberOfParticipants = {
+            abi: contractAbi,
+            contractAddress: deployedAddress,
+            functionName: "getNumberOfPlayers",
+            params: {},
+        };
+
+        const numberOfPlayers = await runContractFunction({
+            params: optionsGetNumberOfParticipants,
+            onError: (error) => console.log(error),
+        });
+
+        setNumberOfPlayers(numberOfPlayers.toString());
     };
 
     useEffect(() => {
         if (isWeb3Enabled) {
-            fetchParticipationFee(chainId).catch(console.error);
+            setChainId(parseInt(chainId).toString());
+            deployedAddress = getLotteryDeployedAddress(chainId);
+            if (deployedAddress != null) {
+                fetchParticipationFee().catch(console.error);
+                fetchNumberOfParticipants();
+            }
         }
     }, [isWeb3Enabled == true]);
 
     useEffect(() => {
         Moralis.onChainChanged(async function (chain) {
-            await fetchParticipationFee(chain).catch(console.error);
+            setChainId(parseInt(chain).toString());
+            deployedAddress = getLotteryDeployedAddress(chain);
+            if (deployedAddress != null) {
+                fetchParticipationFee();
+                fetchNumberOfParticipants();
+            }
         });
     }, []);
 
     return (
         <div>
-            {contractAddress != null ? (
-                <div>
-                    <button
-                        onClick={async function () {
-                            await participateToLottery();
-                        }}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                    >
-                        Participate
-                    </button>
-                    Participation fee :
-                    {ethers.utils.formatUnits(participationFee.toString(), "ether")} ETH
-                </div>
+            {isWeb3Enabled ? (
+                deployedAddress != null ? (
+                    <div>
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                            onClick={async function () {
+                                await participateToLottery();
+                            }}
+                            // disabled={isLoading || isFetching}
+                        >
+                            Participate
+                        </button>
+                        Participation fee :
+                        {ethers.utils.formatUnits(participationFee.toString(), "ether")} ETH
+                        <div>{numberOfPlayers} players are participating</div>
+                    </div>
+                ) : (
+                    <div>
+                        No address found for deployed raffle contract with chainId: {chainIdStr}
+                    </div>
+                )
             ) : (
-                <div>No address found for deployed raffle contract with chainId: {chainIdStr}</div>
+                <div></div>
             )}
         </div>
     );
