140789d27aa56aafe058e6e9547e346a29985f36
Brice Grenard <brice.grenard@gmail.com>
2022-07-23 10:39:49 +0800

    Just some css styling and logic around the participate button to remain disabled when tx is sent or denied

--- components/LotteryParticipation.js
+++ components/LotteryParticipation.js
@@ -13,6 +13,8 @@ export default function Participate() {
     const [participationFee, setParticipationFee] = useState("0");
     const [numberOfPlayers, setNumberOfPlayers] = useState("0");
     const [hasPlayerAlreadyParticipated, setHasPlayerAlreadyParticipated] = useState(false);
+    const [isFetchingHasAlreadyPlayed, setIsFetchingHasAlreadyPlayed] = useState(false);
+    const [isAboutToParticipate, setisAboutToParticipate] = useState(false);
 
     const dispatch = useNotification();
 
@@ -36,10 +38,14 @@ export default function Participate() {
             params: {},
             msgValue: participationFee,
         };
+        setisAboutToParticipate(true);
         await runContractFunction({
             params: optionsParticipate,
             onSuccess: (tx) => handleSuccessTxParticipate(tx),
-            onError: (error) => console.log(error),
+            onError: (error) => {
+                console.log(error);
+                setisAboutToParticipate(false);
+            },
         });
     };
 
@@ -52,6 +58,7 @@ export default function Participate() {
                 await fetchHasAlreadyParticipated(currentAccount);
             }
         } catch (error) {}
+        setisAboutToParticipate(false);
     };
 
     const handleNotificationTransactionCompleted = (tx) => {
@@ -103,11 +110,12 @@ export default function Participate() {
             params: { _address },
         };
 
+        setIsFetchingHasAlreadyPlayed(true);
         const hasAlreadyParticipated = await runContractFunction({
             params: optionsHasAlreadyParticipated,
             onError: (error) => console.log(error),
         });
-
+        setIsFetchingHasAlreadyPlayed(false);
         setHasPlayerAlreadyParticipated(hasAlreadyParticipated);
     };
 
@@ -127,17 +135,15 @@ export default function Participate() {
     };
 
     async function UpdateUI() {
+        setIsFetchingHasAlreadyPlayed(true);
+        setHasPlayerAlreadyParticipated(false);
+
         deployedAddress = getLotteryDeployedAddress();
         if (deployedAddress != null) {
             await fetchParticipationFee();
             await fetchNumberOfParticipants();
             await fetchHasAlreadyParticipated(currentAccount);
         }
-    }
-
-    async function handleAccountChanged(newAccount) {
-        currentAccount = newAccount;
-        await UpdateUI();
     }
 
     async function dispatchWinnerPickedNotification() {
@@ -189,20 +195,50 @@ export default function Participate() {
             {isWeb3Enabled ? (
                 deployedAddress != null ? (
                     <div>
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full disabled:opacity-50 disabled:hover:bg-blue-500"
-                            onClick={async function () {
-                                await participateToLottery();
-                            }}
-                            disabled={hasPlayerAlreadyParticipated}
-                        >
-                            {!hasPlayerAlreadyParticipated
-                                ? "Participate"
-                                : "Wait for the next round..."}
-                        </button>
-                        Participation fee :
-                        {ethers.utils.formatUnits(participationFee.toString(), "ether")} ETH
-                        <div>{numberOfPlayers} player(s) are participating</div>
+                        <div className="p-2 px-14">
+                            {" "}
+                            Participation fee :
+                            {ethers.utils.formatUnits(participationFee.toString(), "ether")} ETH
+                        </div>
+                        <div className=" p-2 px-14">
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded-full disabled:opacity-50 disabled:hover:bg-blue-500  
+                                w-60"
+                                onClick={async function () {
+                                    await participateToLottery();
+                                }}
+                                disabled={
+                                    hasPlayerAlreadyParticipated || isFetchingHasAlreadyPlayed
+                                }
+                            >
+                                {!hasPlayerAlreadyParticipated
+                                    ? "Participate"
+                                    : "Wait for the next round..."}
+                                {isFetchingHasAlreadyPlayed || isAboutToParticipate ? (
+                                    <svg
+                                        class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                        viewBox="0 0 100 101"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                            fill="currentColor"
+                                        />
+                                        <path
+                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                            fill="currentFill"
+                                        />
+                                        <span class="sr-only">Loading...</span>
+                                    </svg>
+                                ) : (
+                                    <div></div>
+                                )}
+                            </button>
+                        </div>
+                        <div className="p-5 px-14">
+                            {numberOfPlayers} player(s) are participating
+                        </div>
                     </div>
                 ) : (
                     <div></div>
