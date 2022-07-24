f5049434d973a5bc41ae3a563e16064d6051e40f
Brice Grenard <brice.grenard@gmail.com>
2022-07-24 21:49:03 +0800

    Bug fixing and refactoring

--- components/LotteryParticipation.js
+++ components/LotteryParticipation.js
@@ -14,7 +14,7 @@ export default function Participate() {
     const [numberOfPlayers, setNumberOfPlayers] = useState("0");
     const [hasPlayerAlreadyParticipated, setHasPlayerAlreadyParticipated] = useState(false);
     const [isFetchingHasAlreadyPlayed, setIsFetchingHasAlreadyPlayed] = useState(false);
-    const [isAboutToParticipate, setisAboutToParticipate] = useState(false);
+    const [waitForParticipatation, setWaitForParticipatation] = useState(false);
 
     const dispatch = useNotification();
 
@@ -38,13 +38,13 @@ export default function Participate() {
             params: {},
             msgValue: participationFee,
         };
-        setisAboutToParticipate(true);
+        setWaitForParticipatation(true);
         await runContractFunction({
             params: optionsParticipate,
             onSuccess: (tx) => handleSuccessTxParticipate(tx),
             onError: (error) => {
                 console.log(error);
-                setisAboutToParticipate(false);
+                setWaitForParticipatation(false);
             },
         });
     };
@@ -58,7 +58,7 @@ export default function Participate() {
                 await fetchHasAlreadyParticipated(currentAccount);
             }
         } catch (error) {}
-        setisAboutToParticipate(false);
+        setWaitForParticipatation(false);
     };
 
     const handleNotificationTransactionCompleted = (tx) => {
@@ -159,31 +159,52 @@ export default function Participate() {
             });
         } catch (error) {}
     }
-    async function SubscribeWinnerPickedEvent() {
-        const ethers = Moralis.web3Library;
-        const provider = await new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(deployedAddress, contractAbi, signer);
 
-        contract.removeAllListeners("WinnerPicked");
-        contract.once("WinnerPicked", async (from) => {
-            await dispatchWinnerPickedNotification();
-            await UpdateUI();
-        });
+    async function OnWinnerPicked() {
+        console.log("OnWinnerPicked event triggered... next, notifying...");
+        await dispatchWinnerPickedNotification();
+        await UpdateUI();
+    }
+
+    function SetButtonTextState() {
+        if (waitForParticipatation) {
+            return "Waiting for confirmation...";
+        } else if (!hasPlayerAlreadyParticipated) {
+            return "Participate";
+        } else return "See you at the next round...";
     }
 
     useEffect(() => {
         if (isWeb3Enabled) {
-            currentAccount = account;
-            UpdateUI();
-            SubscribeWinnerPickedEvent();
+            var contract;
+
+            async function DoUpdate() {
+                currentAccount = account;
+                UpdateUI();
+                const ethers = Moralis.web3Library;
+                const provider = await new ethers.providers.Web3Provider(window.ethereum);
+                const signer = provider.getSigner();
+                contract = new ethers.Contract(deployedAddress, contractAbi, signer);
+                contract.on("WinnerPicked", OnWinnerPicked);
+            }
+
+            DoUpdate();
+
+            return () => {
+                if (contract) {
+                    // console.log("cleaning call");
+                    // console.log(`before ${contract.listenerCount("WinnerPicked")}`);
+                    contract.removeListener("WinnerPicked");
+                    // console.log(`after ${contract.listenerCount("WinnerPicked")}`);
+                }
+            };
         }
     }, [isWeb3Enabled]);
 
     useEffect(() => {
         console.log(`chain changed to ${chainId}`);
-        UpdateUI();
-    }, [isChainIdSupported(chainId)]);
+        if (isChainIdSupported(chainId)) UpdateUI();
+    }, [chainId]);
 
     useEffect(() => {
         currentAccount = account;
@@ -208,15 +229,15 @@ export default function Participate() {
                                     await participateToLottery();
                                 }}
                                 disabled={
-                                    hasPlayerAlreadyParticipated || isFetchingHasAlreadyPlayed
+                                    hasPlayerAlreadyParticipated ||
+                                    isFetchingHasAlreadyPlayed ||
+                                    waitForParticipatation
                                 }
                             >
-                                {!hasPlayerAlreadyParticipated
-                                    ? "Participate"
-                                    : "Wait for the next round..."}
-                                {isFetchingHasAlreadyPlayed || isAboutToParticipate ? (
+                                {SetButtonTextState()}
+                                {isFetchingHasAlreadyPlayed || waitForParticipatation ? (
                                     <svg
-                                        class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                        className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                                         viewBox="0 0 100 101"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -229,7 +250,7 @@ export default function Participate() {
                                             d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                                             fill="currentFill"
                                         />
-                                        <span class="sr-only">Loading...</span>
+                                        <span className="sr-only">Loading...</span>
                                     </svg>
                                 ) : (
                                     <div></div>
